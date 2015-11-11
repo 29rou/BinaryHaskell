edfabee8d5b9b2107d5c77b38286f64a55c55328
29ろう <c171401r@icu.ac.jp>
2015-11-11 15:41:16 +0900

    コードを短くした

--- imghedder.hs
+++ imghedder.hs
@@ -2,6 +2,7 @@ import System.IO (openFile, IOMode(ReadMode))
 import Numeric (showHex)
 import Data.ByteString.Lazy as B (ByteString,  hGetContents, unpack) 
 import GHC.Word (Word8)
+import Data.Char
 
 hex :: Word8 -> String
 hex x = showHex (x) ""
@@ -10,27 +11,8 @@ imgCapture :: [String] -> [String]
 imgCapture (x:y:xs) | x == "ff" && y == "c0" = xs
                     | otherwise = imgCapture xs
 
-hex1 :: Char -> Int
-hex1 x = case x of
-    '0' -> 0
-    '1' -> 1
-    '2' -> 2
-    '3' -> 3
-    '4' -> 4
-    '5' -> 5
-    '6' -> 6
-    '7' -> 7
-    '8' -> 8
-    '9' -> 9
-    'A' -> 10
-    'B' -> 11
-    'C' -> 12
-    'D' -> 13
-    'E' -> 14
-    'F' -> 15
-    
 hex2dec :: [Char] -> Int
-hex2dec (x:xs) = (hex1 x)*(16^(length xs)) + hex2dec(xs)
+hex2dec (x:xs) = (digitToInt x)*(16^(length xs)) + hex2dec(xs)
 hex2dec x = 0
 
 main :: IO ()
