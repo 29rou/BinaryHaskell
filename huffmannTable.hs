acb3bc1f824e328fd4aa1cfee611eff74f063603
29ろう <c171401r@icu.ac.jp>
2015-11-15 01:31:06 +0900

    ビット演算の間違いを修正

--- huffmannTable.hs
+++ huffmannTable.hs
@@ -10,13 +10,13 @@ hex2dec :: [Char] -> Int
 hex2dec (x:xs) = (digitToInt x)*(16^(length xs)) + hex2dec(xs)
 hex2dec x = 0
 
-toBit:: Int -> [Int]
+toBit:: Int -> String
 toBit x = reverse (toBit' x)
 
-toBit' :: Int -> [Int]
-toBit' 1 = [1]
-toBit' 0 = [0]
-toBit' (x) = mod x 2 : toBit' (div x 2) 
+toBit' :: Int -> String
+toBit' 1 = ['1']
+toBit' 0 = ['0']
+toBit' (x) = intToDigit (mod x 2) : toBit' (div x 2) 
 
 
 binaryListMaker :: ByteString -> [String]
@@ -47,17 +47,18 @@ byteGetLoop :: String -> [String]
 byteGetLoop (x1:x2:xs) = ([x1] ++ [x2]) : byteGetLoop xs   
 byteGetLoop (x1) = []
 
-readHuffmanTable :: Int -> Int -> [Int] -> [[[Int]]]
+readHuffmanTable :: Int -> Int -> [Int] -> [[String]]
 readHuffmanTable 0 n (0:xs) = readHuffmanTable 0 (n+1) xs
-readHuffmanTable bits n (0:xs) = readHuffmanTable 0 (n+1) xs
-readHuffmanTable 0 n (x:xs) = map ((replicate (n-1) 0) ++) (bitMaker 0 n x) : readHuffmanTable (shiftL x (n-1)) (n+1) xs
+readHuffmanTable bits n (0:xs) = readHuffmanTable (shiftL (bits) 1) (n+1) xs
+readHuffmanTable 0 n (x:xs) = (bitMaker 0 n x) : readHuffmanTable (shiftL x (n-1)) (n+1) xs
 readHuffmanTable bits n (x:xs) = (bitMaker (bits) n x) : readHuffmanTable (shiftL (bits+x) 1) (n+1) xs
 readHuffmanTable _ _ x = []
 
-bitMaker :: Int -> Int-> Int -> [[Int]]
-bitMaker 0 n y = ( toBit 0) : bitMaker (1) n (y-1)
-bitMaker x n 0 = []
-bitMaker x n y = toBit x : bitMaker (x+1) n (y-1)
+bitMaker :: Int -> Int-> Int -> [String]
+bitMaker 0 n y = (replicate (n) '0') : bitMaker (1) n (y-1)
+bitMaker _ _ 0 = []
+bitMaker x n y = concat ([replicate (n- length bitData) '0'] ++ [bitData]) : bitMaker (x+1) n (y-1)
+                 where bitData = toBit x
 
 main :: IO ()
 main = do
@@ -70,11 +71,14 @@ main = do
         huffmanTable = convertHuffmanTable $ markerFilter "ffc4" binaryList
     --print (length (head huffmanTable))
     let test = (huffmanTable)
-    print (take 16 (drop 3 (head huffmanTable)))
-    print (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (head huffmanTable))))
-    saveFile <- openFile "test2.txt" WriteMode
+    saveFile <- openFile "test3.txt" WriteMode
+    --hPrint (take 16 (drop 3 (head huffmanTable)))
     hPutStrLn saveFile (show (head test))
+    hPrint saveFile (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (head huffmanTable))))
     hPutStrLn saveFile (show (test !! 1))
+    hPrint saveFile (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (huffmanTable!!1))))
     hPutStrLn saveFile (show (test !! 2))
+    hPrint saveFile (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (huffmanTable!!2))))
     hPutStrLn saveFile (show (test !! 3))
+    hPrint saveFile (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (huffmanTable!!3))))
     hClose saveFile
