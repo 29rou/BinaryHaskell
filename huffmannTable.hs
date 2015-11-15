869400418542e24ab8c7b485ed8dc4278f60ae90
29ろう <c171401r@icu.ac.jp>
2015-11-16 02:36:30 +0900

    ハフマン符号とデータの対応付け（DC成分のみ）

--- huffmannTable.hs
+++ huffmannTable.hs
@@ -40,12 +40,18 @@ byteLoder x y = [y !! (x*2) ] ++ [y !! ((x*2)+1)]
 convertHuffmanTable :: [String] -> [[Int]]
 convertHuffmanTable (x:xs) = map hex2dec (analyzeHuffmanTable x) : convertHuffmanTable xs
     where analyzeHuffmanTable :: String -> [String]
-          analyzeHuffmanTable x = [(byteLoder 2 x)++(byteLoder 3 x)]++[[(x !! 8)]]++[[(x !! 9)]]++byteGetLoop(drop 10 x)
+          analyzeHuffmanTable x = [(byteLoder 2 x)++(byteLoder 3 x)]++[[(x !! 8)]]++[[(x !! 9)]]++(byteGetLoop (digitToInt(x !! 8)) (drop 10 x))
 convertHuffmanTable x = []
 
-byteGetLoop :: String -> [String]
-byteGetLoop (x1:x2:xs) = ([x1] ++ [x2]) : byteGetLoop xs   
-byteGetLoop (x1) = []
+byteGetLoop :: Int -> String -> [String]
+byteGetLoop 0 (x1:x2:xs) = ([x1] ++ [x2]) : byteGetLoop 0 xs   
+byteGetLoop 0 x = []
+byteGetLoop 15 (x1:x2:xs) =  ([x1] ++ [x2]) : (bitGetLoop xs)
+byteGetLoop n (x1:x2:xs) = ([x1] ++ [x2]) : byteGetLoop (n+1) xs
+
+bitGetLoop :: String -> [String]
+bitGetLoop (x:xs) = [x] : bitGetLoop xs   
+bitGetLoop x = []
 
 readHuffmanTable :: Int -> Int -> [Int] -> [[String]]
 readHuffmanTable 0 n (0:xs) = readHuffmanTable 0 (n+1) xs
@@ -53,6 +59,9 @@ readHuffmanTable bits n (0:xs) = readHuffmanTable (shiftL (bits) 1) (n+1) xs
 readHuffmanTable 0 n (x:xs) = (bitMaker 0 n x) : readHuffmanTable (shiftL x (n-1)) (n+1) xs
 readHuffmanTable bits n (x:xs) = (bitMaker (bits) n x) : readHuffmanTable (shiftL (bits+x) 1) (n+1) xs
 readHuffmanTable _ _ x = []
+
+readHuffmanTable' :: Int -> Int -> [Int] -> [(String,Int)]
+readHuffmanTable' x y z = zip (concat $ readHuffmanTable x y (take 16 (drop 3 z)))  (drop 19 z)
 
 bitMaker :: Int -> Int-> Int -> [String]
 bitMaker 0 n y = (replicate (n) '0') : bitMaker (1) n (y-1)
@@ -69,16 +78,18 @@ main = do
         quantizationTable = markerFilter "ffdb" binaryList
         metaData = markerFilter "ffc0" binaryList
         huffmanTable = convertHuffmanTable $ markerFilter "ffc4" binaryList
-    --print (length (head huffmanTable))
+    print (head huffmanTable)
     let test = (huffmanTable)
     saveFile <- openFile "test3.txt" WriteMode
     --hPrint (take 16 (drop 3 (head huffmanTable)))
+    --putStrLn  (show (head test))
+    --print  (readHuffmanTable' 0 1  (head huffmanTable))
     hPutStrLn saveFile (show (head test))
-    hPrint saveFile (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (head huffmanTable))))
+    hPrint  saveFile (readHuffmanTable' 0 1  (head huffmanTable))
     hPutStrLn saveFile (show (test !! 1))
-    hPrint saveFile (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (huffmanTable!!1))))
+    hPrint saveFile ( readHuffmanTable' 0 1 (huffmanTable!!1))
     hPutStrLn saveFile (show (test !! 2))
-    hPrint saveFile (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (huffmanTable!!2))))
+    hPrint saveFile (readHuffmanTable' 0 1 (huffmanTable!!2))
     hPutStrLn saveFile (show (test !! 3))
-    hPrint saveFile (concat $ readHuffmanTable 0 1 (take 16 (drop 3 (huffmanTable!!3))))
+    hPrint saveFile (readHuffmanTable' 0 1 (huffmanTable!!3))
     hClose saveFile
